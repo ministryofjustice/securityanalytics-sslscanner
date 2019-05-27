a45fd4fa5e42a34adbcc61fdf8db694e1468d0c0
Phil Rogers <47106782+progers-SL@users.noreply.github.com>
2019-05-27 15:42:01 +0100

    removed unused files

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -39,11 +39,6 @@ variable "known_deployment_stages" {
   default = ["dev", "qa", "prod"]
 }
 
-variable "scan_hosts" {
-  type    = "list"
-  default = ["scanme.nmap.org"]
-}
-
 provider "aws" {
   region              = "${var.aws_region}"
   profile             = "${var.app_name}"
@@ -62,25 +57,10 @@ locals {
   ssm_source_stage = "${var.ssm_source_stage == "DEFAULT" ? terraform.workspace : var.ssm_source_stage}"
 }
 
-# module "docker_image" {
-#   source             = "docker_image"
-#   app_name           = "${var.app_name}"
-#   task_name          = "${var.task_name}"
-#   results_bucket_arn = "${module.ssl_task.results_bucket_arn}"
-#   results_bucket_id  = "${module.ssl_task.results_bucket_id}"
-#   ssm_source_stage   = "${local.ssm_source_stage}"
-# }
-
-module "elastic_resources" {
-  source           = "elastic_resources"
-  aws_region       = "${var.aws_region}"
-  app_name         = "${var.app_name}"
-  task_name        = "${var.task_name}"
-  ssm_source_stage = "${local.ssm_source_stage}"
-}
-
 module "ssl_task" {
-  // two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories  # source = "github.com/ministryofjustice/securityanalytics-taskexecution//infrastructure/ecs_task"
+  // two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories  
+
+  # source = "github.com/ministryofjustice/securityanalytics-taskexecution//infrastructure/ecs_task"
 
   // It is sometimes useful for the developers of the project to use a local version of the task
   // execution project. This enables them to develop the task execution project and the ssl scanner
@@ -89,40 +69,14 @@ module "ssl_task" {
   // devs will have to comment in/out this line as and when they need
   source = "../../securityanalytics-taskexecution/infrastructure/task"
 
-  app_name   = "${var.app_name}"
-  aws_region = "${var.aws_region}"
-
-  # cpu        = "1024"
-  # memory     = "2048"
-
-  # docker_dir                    = "${dirname(module.docker_image.docker_file)}"
-  task_name = "${var.task_name}"
-  # sources_hash                  = "${module.docker_image.sources_hash}"
-  # docker_hash                   = "${module.docker_image.docker_hash}"
+  app_name                      = "${var.app_name}"
+  aws_region                    = "${var.aws_region}"
+  task_name                     = "${var.task_name}"
   subscribe_elastic_to_notifier = true
-  account_id          = "${var.account_id}"
-  ssm_source_stage    = "${local.ssm_source_stage}"
-  transient_workspace = "${!contains(var.known_deployment_stages, terraform.workspace)}"
+  account_id                    = "${var.account_id}"
+  ssm_source_stage              = "${local.ssm_source_stage}"
+  transient_workspace           = "${!contains(var.known_deployment_stages, terraform.workspace)}"
 }
-
-# module "ssl_task_scheduler" {
-#   // two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories
-#   source = "github.com/ministryofjustice/securityanalytics-taskexecution//infrastructure/scheduler"
-
-#   // It is sometimes useful for the developers of the project to use a local version of the task
-#   // execution project. This enables them to develop the task execution project and the ssl scanner
-#   // (or other future tasks), at the same time, without requiring the task execution changes to be
-#   // pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
-#   // devs will have to comment in/out this line as and when they need
-#   // source = "../../securityanalytics-taskexecution/infrastructure/scheduler"
-
-#   app_name            = "${var.app_name}"
-#   task_name           = "${var.task_name}"
-#   scan_hosts          = "${var.scan_hosts}"
-#   queue_url           = "${module.ssl_task.task_queue_url}"
-#   queue_arn           = "${module.ssl_task.task_queue}"
-#   transient_workspace = "${!contains(var.known_deployment_stages, terraform.workspace)}"
-# }
 
 module "openssl" {
   source                   = "ssl_lambdas"
