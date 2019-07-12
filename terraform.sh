5d867f64af9028f008802567c34a891811a7f7be
Phil Rogers <47106782+progers-SL@users.noreply.github.com>
2019-07-12 09:42:41 +0100

    pipenv clean and update terraform

--- terraform.sh
+++ terraform.sh
@@ -3,6 +3,7 @@
 cd infrastructure
 terraform init -backend-config "bucket=$1-terraform-state"
 terraform workspace new $2 || terraform workspace select $2
+terraform get --update
 terraform apply -auto-approve -input=true -var app_name=$1
 # pause in case the user is watching output
 sleep 5
